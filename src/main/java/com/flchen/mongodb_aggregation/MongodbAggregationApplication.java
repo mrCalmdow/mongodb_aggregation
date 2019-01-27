a3d5d92fb23b0474bdb1bea14d7fd59d4e912d1a
flchen <634233816@qq.com>
2019-01-27 09:05:29 +0800

    init project

---
+++ src/main/java/com/flchen/mongodb_aggregation/MongodbAggregationApplication.java
@@ -0,0 +1,14 @@
+package com.flchen.mongodb_aggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbAggregationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbAggregationApplication.class, args);
+    }
+
+}
+
